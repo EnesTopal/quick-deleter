b0c7a49558e28ccd1da7967a6c42f7cc6a50906f
EnesTopal <enes.topal1@gazi.edu.tr>
2025-05-06 12:12:33 +0300

    deleting and basic app created

--- app/src/main/java/com/example/quickdeleter/MainActivity.kt
+++ app/src/main/java/com/example/quickdeleter/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             QuickdeleterTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                FileListScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    QuickdeleterTheme {
-        Greeting("Android")
-    }
-}
